81004206adf59f917f043a1f3a74ee8eded072f2
porchsal <porchsal@yahoo.com>
2023-06-29 23:09:03 -0400

    InterviewerList Component

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -3,7 +3,6 @@ import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Button from "components/Button";
-import classNames from "classnames";
 
 afterEach(cleanup);
 
