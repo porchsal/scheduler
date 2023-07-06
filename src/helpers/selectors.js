fa128dcf01ade571dc48e3cddcd2a4cb13c91b4c
porchsal <porchsal@yahoo.com>
2023-07-05 21:39:15 -0400

    create selector and test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+
+
+export function getAppointmentsForDay(state, day) {
+    let result = [];
+    let appointmentsForDay = state.days.filter(e => e.name === day);
+    
+    if (appointmentsForDay.length === 0){ 
+        console.log(result);
+        return result;}
+    for (const a of appointmentsForDay[0].appointments) {
+    result.push(state.appointments[a]);
+        }
+    console.log(appointmentsForDay);
+    return result;
+};
