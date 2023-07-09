ccc553f26058ba60b0333dfd28cfe27a3a2c1de9
porchsal <porchsal@yahoo.com>
2023-07-08 21:39:37 -0400

    Implementing Hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,34 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import React from 'react';
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
 
 export default function Appointment(props) {
+    const EMPTY = "EMPTY";
+    const SHOW = "SHOW";
+    const CREATE = "CREATE";
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
+      );
+    
     return (
         <article className="appointment">
             <Header time={props.time} />
-            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />}
+            {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+            {mode === CREATE && (
+                <Form
+                    interviewers={[]}
+                    onCancel={() => back()}
+                />
+            )}
+            {mode === SHOW && (
+                <Show
+                    student={props.interview.student}
+                    interviewer={props.interview.interviewer}
+                />
+            )}
         </article>
     )
 }
