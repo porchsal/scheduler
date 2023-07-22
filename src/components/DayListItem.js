03a0e2d72cfc9c74046adaf1f6ac3624109cb9ed
porchsal <porchsal@yahoo.com>
2023-07-22 00:38:27 -0400

    Testing with cypress

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,8 @@ export default function DayListItem(props) {
       }
 
     return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day">
+    <li className={dayClass} onClick={() => props.setDay(props.name)} 
+      selected={props.selected} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
